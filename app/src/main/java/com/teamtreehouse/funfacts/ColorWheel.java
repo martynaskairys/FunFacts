12f1159038dcb78b016b2cfa137b5a8f85040430
martynaskairys <martynaskairys@gmail.com>
2015-07-24 22:56:05 +0300

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -0,0 +1,41 @@
+package com.teamtreehouse.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by Martynas on 2015-07-04.
+ */
+public class ColorWheel {
+    public String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray };
+    };
+
+    // Method (abilities: things objects can do)
+    public int getColor() {
+
+        String color = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random(); //construct a new random number generator
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+
+        color = mColors[randomNumber];
+
+
+        return Color.parseColor(color);
+    }
+}
